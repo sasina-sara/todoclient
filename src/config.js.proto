c43e347d00791d90d8b90424a0cce379ffa23b1c
sasina-sara <s5835512016@phuket.psu.ac.th>
2020-03-19 12:33:16 +0700

    Initial commit

---
+++ src/config.js.proto
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+
+export default config;
